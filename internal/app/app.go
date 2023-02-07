6c9c2a5aed8afb00872b2837a0387185912369d1
Alexey Bitkov <alxsmrn@yandex.ru>
2023-02-07 21:20:02 +0300

    Add handler for server

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,10 +1,19 @@
 package app
 
 import (
+	"github.com/Albitko/shortener/internal/controller"
+	"github.com/Albitko/shortener/internal/usecase"
+	"github.com/Albitko/shortener/internal/usecase/repo"
 	"log"
 	"net/http"
 )
 
 func Run() {
+	repository := repo.NewRepository()
+	uc := usecase.NewUrlConverter(repository)
+	handler := controller.NewUrlHandler(uc)
+
+	http.Handle("/", handler)
+
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
