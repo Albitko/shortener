d42f825de218a7b76bc28c9113f3501a2fb0e441
Alexey Bitkov <alxsmrn@yandex.ru>
2023-05-27 10:12:37 +0300

    add benchmarks

--- internal/controller/handlers_bench_test.go
+++ internal/controller/handlers_bench_test.go
@@ -7,13 +7,59 @@ import (
 	"github.com/google/uuid"
 )
 
-const postMainURL = "http://localhost:8080/"
+const mainURL = "http://localhost:8080/"
+const shortenURL = "http://localhost:8080/api/shorten"
+const shortenBatchURL = "http://localhost:8080/api/shorten/batch"
+const userURL = "http://localhost:8080/api/user/urls"
 
-func BenchmarkPostMain(b *testing.B) {
+func BenchmarkHandlers(b *testing.B) {
 	client := resty.New()
 	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		id := uuid.New().String()
-		client.R().SetBody("http://" + id).Post(postMainURL)
-	}
+	b.Run("POST /", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			b.StopTimer()
+			id := uuid.New().String()
+			b.StartTimer()
+			client.R().SetBody("http://" + id).Post(mainURL)
+		}
+	})
+	b.Run("POST /api/shorten", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			b.StopTimer()
+			id := uuid.New().String()
+			b.StartTimer()
+			client.R().SetBody(`{"url":"https://` + id + `"}`).Post(shortenURL)
+		}
+	})
+	b.Run("POST /api/shorten/batch", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			b.StopTimer()
+			correlationID := uuid.New().String()
+			firstSite := uuid.New().String()
+			secondSite := uuid.New().String()
+			b.StartTimer()
+			client.R().SetBody(
+				`[{"correlation_id": "` + correlationID + `", "original_url": "https://` + firstSite +
+					`"},{"correlation_id": "` + correlationID + `", "original_url": "https://` + secondSite + `"}]`,
+			).Post(shortenBatchURL)
+		}
+	})
+	b.Run("GET /:id", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			b.StopTimer()
+			id := uuid.New().String()
+			b.StartTimer()
+			client.R().Get(mainURL + id)
+		}
+	})
+	b.Run("GET /api/user/urls", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			b.StopTimer()
+			id := uuid.New().String()
+			client.R().SetBody("http://" + id).Post(mainURL)
+			b.StartTimer()
+			client.R().Get(userURL)
+		}
+	})
+
 }
